2fdcabfa33ebf07baec988efdd17ad965cfe5021
Yohannes Alemu <johannesalemu01@gmail.com>
2026-03-31 06:14:46 +0300

    feat: create UserRepository for profile and preference management

---
+++ lib/providers/user/user_repository.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'package:dine_ease/services/api_service.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserRepository {
+  final ApiService _apiService = ApiService();
+
+  Future<Map<String, dynamic>?> getProfile() async {
+    final response = await _apiService.get('/users/profile');
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    }
+    return null;
+  }
+
+  Future<bool> updateProfile(Map<String, dynamic> data) async {
+    final response = await _apiService.patch('/users/profile', data);
+    return response.statusCode == 200;
+  }
+
+  Future<bool> updatePreferences(Map<String, dynamic> prefs) async {
+    final response = await _apiService.patch('/users/preferences', prefs);
+    return response.statusCode == 200;
+  }
+
+  Future<bool> deleteAccount() async {
+    final response = await _apiService.delete('/users/account');
+    return response.statusCode == 200;
+  }
+}
+
+final userRepositoryProvider = Provider((ref) => UserRepository());
